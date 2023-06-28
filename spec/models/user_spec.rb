30a7beec6a46667908e30ef5f49b584772da85cb
Matt Enyeart <menyeart1@gmail.com>
2023-06-28 15:25:57 -0600

    spec:correct model attribute naming error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name}
     it { should validate_presence_of :email}
     it { should validate_presence_of :password_digest}
-    it { should validate_presence_of :phone_number}
+    it { should validate_presence_of :phone}
     it { should validate_uniqueness_of :email}
-    it { should validate_uniqueness_of :phone_number}
+    it { should validate_uniqueness_of :phone}
   end
 end
