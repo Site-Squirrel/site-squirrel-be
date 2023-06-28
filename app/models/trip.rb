23097e2464dec58b2b53ffaef14194c77fc3e9d5
Matt Enyeart <menyeart1@gmail.com>
2023-06-28 15:24:11 -0600

    feat:add initial relations and validations to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,7 @@
 class Trip < ApplicationRecord
+  belongs_to :user
+  has_many :reservation_days
+
+
+  validates :vehicle_length, :campground_id, :tent_site_ok, :name, :campground_location, :number_nights, presence: true
 end
