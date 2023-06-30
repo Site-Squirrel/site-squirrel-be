0422d00459ee0a6dd4874a952c8134537acc6cf4
Matt Enyeart <menyeart1@gmail.com>
2023-06-30 14:11:03 -0600

    spec:create campsite details service spec

---
+++ spec/services/campsite_service_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe CampsiteService do
+  before(:each) do
+    @service = CampsiteService.new("906")
+  end
+
+  describe "exists and has an api call", :vcr do
+    it "is a service" do
+      expect(@service).to be_a(CampsiteService)
+    end
+
+    it "has a connection to faraday" do
+      expect(@service.conn).to be_a(Faraday::Connection)
+    end
+
+    it "returns a hash of campground availability data" do
+      result = @service.get_campsite_attributes
+
+      expect(result.first).to have_key(:CampsiteID)
+      expect(result.first).to have_key(:CampsiteLatitude)
+      expect(result.first).to have_key(:CampsiteLongitude)
+      expect(result.first).to have_key(:CampsiteName)
+      expect(result.first).to have_key(:CampsiteAccessible)
+      expect(result.first).to have_key(:CampsiteType)
+      expect(result.first).to have_key(:CampsiteType)
+      expect(result.first).to have_key(:CampsiteType)
+      expect(result.first[:ATTRIBUTES]).to be_a(Array)
+    end
+  end
+end
