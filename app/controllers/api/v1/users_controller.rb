45a77a2063ee707a5f9b34db3369d9295c516b2e
Matt Enyeart <menyeart1@gmail.com>
2023-07-03 15:19:27 -0600

    feat:create users controller and create action

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    if params[:password_digest] == params[:password_confirmation]
+      user = User.new(user_params)
+      user.save!
+      render json: UserSerializer.new(user), status: 201
+    else
+      render json: ErrorSerializer.new("Passwords do not match").invalid_request, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :name, :password_digest, :phone)
+  end
+end
