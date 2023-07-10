c0db40bcb27d46d14b5351b7c1beaede1e862965
Matt Enyeart <menyeart1@gmail.com>
2023-07-10 15:50:59 -0600

    fix: refactor trip model validation for boolean

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,6 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :reservation_days
 
-  validates :vehicle_length, :campground_id, :tent_site_ok, :name, :campground_location, :number_nights, presence: true
+  validates :vehicle_length, :campground_id, :name, :campground_location, :number_nights, presence: true
+  validates :tent_site_ok, inclusion: { in: [true, false] }
 end
