1aaae3bd1fb7f7723c17b6251e10eae974742592
Matt Enyeart <menyeart1@gmail.com>
2023-07-10 15:51:40 -0600

    feat:create user trips index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[create update destroy]
+      resources :users, only: %i[create update destroy] do
+        resources :trips, only: %i[index], controller: "users/trips"
+      end
       resources :sessions, only: [:create]
     end
   end
