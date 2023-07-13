0b64ae4efb8d1d667d7c95eada7b5ce4414bdcc9
Matt Enyeart <menyeart1@gmail.com>
2023-07-13 15:53:27 -0600

    fix:update reservation day schema attributes

--- db/migrate/20230626203957_create_reservation_days.rb
+++ db/migrate/20230626203957_create_reservation_days.rb
@@ -2,11 +2,12 @@ class CreateReservationDays < ActiveRecord::Migration[7.0]
   def change
     create_table :reservation_days do |t|
       t.string :site_number
-      t.string :loop_number
+      t.string :loop
       t.string :checkout_time
-      t.decimal :price
+      t.string :checkin_time
       t.boolean :search_active, default: true
       t.string :date
+      t.string :api_id
 
       t.timestamps
     end
