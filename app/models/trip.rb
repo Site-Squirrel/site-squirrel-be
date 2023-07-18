f9b3380131b576d550061c6420dbc7ceaa505c60
Matt Enyeart <menyeart1@gmail.com>
2023-07-18 15:57:34 -0600

    feat:add dependent destroy for reservation days

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :reservation_days
+  has_many :reservation_days, dependent: :destroy
 
   validates :vehicle_length, :campground_id, :name, :campground_location, :number_nights, :user_id, presence: true
 end
