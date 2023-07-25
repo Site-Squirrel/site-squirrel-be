7f9308b6a302206f9343acf696a60f1ac2a37ea7
Matt Enyeart <menyeart1@gmail.com>
2023-07-25 14:36:52 -0600

    feat:create method for getting res days with active search

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
   has_many :reservation_days
 
   validates :vehicle_length, :campground_id, :name, :campground_location, :number_nights, :user_id, presence: true
+
+  def active_days
+    reservation_days.where(search_active: :true)
+  end
 end
