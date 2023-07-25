f10267efddf03f074a4d232f170d3894a87b6006
Matt Enyeart <menyeart1@gmail.com>
2023-07-25 14:37:41 -0600

    refactor:change email module allow method inputs

--- lib/modules/email.rb
+++ lib/modules/email.rb
@@ -1,14 +1,13 @@
 module Email
 
-  from = SendGrid::Email.new(email: 'test@example.com')
-  to = SendGrid::Email.new(email: 'menyeart1@gmail.com')
-  subject = 'Sending with Twilio SendGrid is Fun'
-  content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-  @@mail = SendGrid::Mail.new(from, subject, to, content)
+  def send_email(to, subject, email_text)
 
-  def send_email
-  sg = SendGrid::API.new(api_key: ENV['TWIL_KEY'])
-  response = sg.client.mail._('send').post(request_body: @@mail.to_json)
-  binding.pry
+    from = SendGrid::Email.new(email: 'menyeart22@turing.edu')
+    to = SendGrid::Email.new(email: to)  
+    content = SendGrid::Content.new(type: 'text/plain', value: email_text)
+    
+    mail = SendGrid::Mail.new(from, subject, to, content)
+    sg = SendGrid::API.new(api_key: ENV['SEND_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
   end
 end
