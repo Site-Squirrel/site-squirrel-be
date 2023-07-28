50090490bbdb78d23c9eed80aa4d0d94c540d96e
Matt Enyeart <menyeart1@gmail.com>
2023-07-28 08:12:50 -0600

    chore:rubocop linting

--- lib/modules/email.rb
+++ lib/modules/email.rb
@@ -1,11 +1,9 @@
 module Email
-
   def send_email(to, subject, email_text)
-
     from = SendGrid::Email.new(email: 'menyeart22@turing.edu')
-    to = SendGrid::Email.new(email: to)  
+    to = SendGrid::Email.new(email: to)
     content = SendGrid::Content.new(type: 'text/plain', value: email_text)
-    
+
     mail = SendGrid::Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SEND_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
--- spec/requests/api/v1/users/trips_request_spec.rb
+++ spec/requests/api/v1/users/trips_request_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "User Trips Request" do
-  describe "User Trips Index" do
+RSpec.describe 'User Trips Request' do
+  describe 'User Trips Index' do
     it "can render all of a user's trips" do
       data_keys = %i[id type attributes]
       attribute_keys = %i[name email phone role trips]
-      trip_attribute_keys = [:name, :campground_id, :vehicle_length, :tent_site_ok, :campground_location, :start_date, :number_nights]
+      trip_attribute_keys = %i[name campground_id vehicle_length tent_site_ok campground_location start_date
+                               number_nights]
       user_1 = create(:user)
       trip_1 = create(:trip, user_id: user_1.id)
       trip_2 = create(:trip, user_id: user_1.id)
       trip_3 = create(:trip, user_id: user_1.id)
-    
+
       get "/api/v1/users/#{user_1.id}/trips"
 
       user = JSON.parse(response.body, symbolize_names: true)
@@ -37,15 +38,15 @@ RSpec.describe "User Trips Request" do
       expect(user[:data][:attributes][:trips][:data].first[:attributes][:number_nights]).to eq(trip_1.number_nights)
     end
 
-    it "can render a user with no trips" do
+    it 'can render a user with no trips' do
       data_keys = %i[id type attributes]
       attribute_keys = %i[name email phone role trips]
       user_1 = create(:user)
-    
+
       get "/api/v1/users/#{user_1.id}/trips"
 
       user = JSON.parse(response.body, symbolize_names: true)
-  
+
       expect(response).to be_successful
       expect(response.status).to eq(200)
       expect(user[:data].keys).to eq(data_keys)
@@ -58,10 +59,10 @@ RSpec.describe "User Trips Request" do
       expect(user[:data][:attributes][:trips][:data]).to eq([])
     end
 
-    it "can render an error if the user is not found" do
+    it 'can render an error if the user is not found' do
       error_keys = %i[status title detail]
-    
-      get "/api/v1/users/15/trips"
+
+      get '/api/v1/users/15/trips'
 
       error = JSON.parse(response.body, symbolize_names: true)
 
@@ -76,11 +77,12 @@ RSpec.describe "User Trips Request" do
     end
   end
 
-  describe "User Trips create" do
-    it "can create a new trip" do
+  describe 'User Trips create' do
+    it 'can create a new trip' do
       user_1 = create(:user)
-      trip_keys = [:id, :type, :attributes]
-      trip_attr_keys = [:name, :campground_id, :vehicle_length, :tent_site_ok, :campground_location, :start_date, :number_nights]
+      trip_keys = %i[id type attributes]
+      trip_attr_keys = %i[name campground_id vehicle_length tent_site_ok campground_location start_date
+                          number_nights]
       headers = { 'CONTENT_TYPE' => 'application/json' }
 
       body =  {
@@ -93,15 +95,15 @@ RSpec.describe "User Trips Request" do
         'campground_id': '123456',
         'campground_location': '123213.23, 123232,12',
         'user_id': "#{user_1.id}"
-              }
+      }
 
-      expect(Trip.count).to eq(0) 
-      expect(ReservationDay.count).to eq(0)       
+      expect(Trip.count).to eq(0)
+      expect(ReservationDay.count).to eq(0)
 
       post "/api/v1/users/#{user_1.id}/trips", headers:, params: JSON.generate(body)
-      
+
       trip = JSON.parse(response.body, symbolize_names: true)
-       
+
       expect(Trip.count).to eq(1)
       expect(ReservationDay.count).to eq(2)
       expect(response).to be_successful
@@ -116,7 +118,7 @@ RSpec.describe "User Trips Request" do
       expect(trip[:data][:attributes][:number_nights]).to eq(body[:number_nights])
     end
 
-    it "renders an error if a required field is missing" do
+    it 'renders an error if a required field is missing' do
       user_1 = create(:user)
       error_keys = %i[status title detail]
       headers = { 'CONTENT_TYPE' => 'application/json' }
@@ -131,12 +133,12 @@ RSpec.describe "User Trips Request" do
         'campground_id': '123456',
         'campground_location': '123213.23, 123232,12',
         'user_id': "#{user_1.id}"
-              }
+      }
 
       post "/api/v1/users/#{user_1.id}/trips", headers:, params: JSON.generate(body)
-      
+
       error = JSON.parse(response.body, symbolize_names: true)
-  
+
       expect(response).to_not be_successful
       expect(response.status).to eq(400)
       expect(error).to have_key(:errors)
